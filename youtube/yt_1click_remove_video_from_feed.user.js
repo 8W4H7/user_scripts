def0edbf481d5ed1eb9cf14d79fff698f79351dc
8W4H7 <64033612+8W4H7@users.noreply.github.com>
2020-07-11 21:16:28 +0400

    Update: Condition "NOT new UI (2017)"

--- youtube/yt_1click_remove_video_from_feed.user.js
+++ youtube/yt_1click_remove_video_from_feed.user.js
@@ -5,7 +5,7 @@
 // @icon        https://www.youtube.com/yts/img/favicon_96-vflW9Ec0w.png
 // @homepageURL https://github.com/8W4H7/user_scripts/
 // @downloadURL https://raw.githubusercontent.com/8W4H7/user_scripts/master/youtube/yt_1click_remove_video_from_feed.user.js
-// @version     2020.05.25
+// @version     2020.07.11
 // @author      8W4H7
 // @license     MIT
 // @match       http*://*youtube.com/feed/subscriptions
@@ -14,7 +14,7 @@
 "use strict";
 (function() {
   // [Condition]: NOT new UI (2017)
-  if (document.cookie.includes("PREF=f6=8&f5=") !== true) return console.log('[YT: 1 Click remove video from related]: Not for new UI (2017), sorry :(');
+  if (document.cookie.includes("PREF=f6=") !== true) return console.log('[YT: 1 Click remove video from related]: Not for new UI (2017), sorry :(');
   
   function oneClick() {
     const feedItems = document.querySelectorAll('div.yt-lockup.yt-lockup-grid.yt-lockup-video.clearfix');
